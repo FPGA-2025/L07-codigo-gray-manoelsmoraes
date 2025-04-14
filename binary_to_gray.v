6eb48e2aca4539f211791543dce37e9006f5e6e3
manoelsmoraes <manoel@msminfo.com.br>
2025-04-14 18:17:29 -0300

    Lab07 ok

--- binary_to_gray.v
+++ binary_to_gray.v
@@ -5,4 +5,10 @@ module Binary_to_Gray (
 
 // Insira seu codigo aqui
 
+
+    assign gray_out[3] = binary_in[3];
+    assign gray_out[2] = binary_in[3] ^ binary_in[2];
+    assign gray_out[1] = binary_in[2] ^ binary_in[1];
+    assign gray_out[0] = binary_in[1] ^ binary_in[0];
+
 endmodule
